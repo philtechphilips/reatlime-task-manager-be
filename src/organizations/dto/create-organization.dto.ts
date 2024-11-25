e52739b438eed8c5ce03f567a5fe8c3291fa4eda
Isola Pelumi <pelumiisola87@gmail.com>
2024-11-25 23:17:29 +0100

    feat: swagger examples added

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateOrganizationDto {
+  @ApiProperty({
+    example: 'Xttreme Dev',
+    description: 'The name of the organization',
+  })
   @IsString()
   @IsNotEmpty()
   @Length(1, 255)
